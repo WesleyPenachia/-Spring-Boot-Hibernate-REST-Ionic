85274c20dc67f2e54f172308d5c345ead2a61d2a
Wesley Penachia <wesleyikiko@hotmail.com>
2021-09-24 00:30:16 -0300

    Validacao bean

--- src/main/java/com/natymorgs/backEndSpring/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/natymorgs/backEndSpring/resources/exception/ResourceExceptionHandler.java
@@ -1,20 +1,29 @@
 package com.natymorgs.backEndSpring.resources.exception;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpServerErrorException;
 
+import com.natymorgs.backEndSpring.services.exceptions.DataIntegretyException;
 import com.natymorgs.backEndSpring.services.exceptions.ObjecNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ObjecNotFoundException.class)
-	public ResponseEntity<StandardError> objectNotFound(ObjecNotFoundException e, HttpServerErrorException request){
+	public ResponseEntity<StandardError> objectNotFound(ObjecNotFoundException e, HttpServletRequest request){
 		
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+	}
+		@ExceptionHandler(DataIntegretyException.class)
+		public ResponseEntity<StandardError> dataintegrety(DataIntegretyException e, HttpServletRequest request){
+			
+			StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
