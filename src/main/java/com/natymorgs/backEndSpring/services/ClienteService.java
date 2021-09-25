f7c390cc7aaf958387b6c88cc7b422229118dcad
Wesley Penachia <wesleyikiko@hotmail.com>
2021-09-24 23:29:24 -0300

    Correcoes e put

--- src/main/java/com/natymorgs/backEndSpring/services/ClienteService.java
+++ src/main/java/com/natymorgs/backEndSpring/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.natymorgs.backEndSpring.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.natymorgs.backEndSpring.domain.Cliente;
+import com.natymorgs.backEndSpring.dto.ClienteDTO;
 import com.natymorgs.backEndSpring.repositories.ClienteRepository;
+import com.natymorgs.backEndSpring.services.exceptions.DataIntegretyException;
 import com.natymorgs.backEndSpring.services.exceptions.ObjecNotFoundException;
 
 @Service
@@ -20,4 +27,38 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjecNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		} 
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegretyException("Não é possível excluir devido o dado possuir valores referenciados em outra tabela");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO ) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 }
