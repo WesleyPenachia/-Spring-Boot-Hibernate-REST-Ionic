f9fd899392f36dd915aabdf78654fa3a0c2c8ed8
Wesley Penachia <wesleyikiko@hotmail.com>
2021-09-25 01:23:36 -0300

    Validacao email

--- src/main/java/com/natymorgs/backEndSpring/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/natymorgs/backEndSpring/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.natymorgs.backEndSpring.domain.Cliente;
 import com.natymorgs.backEndSpring.domain.enums.TipoCliente;
 import com.natymorgs.backEndSpring.dto.ClienteNewDTO;
+import com.natymorgs.backEndSpring.repositories.ClienteRepository;
 import com.natymorgs.backEndSpring.resources.exception.FieldMessage;
 import com.natymorgs.backEndSpring.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -25,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfouCnpj())) {
 			list.add(new FieldMessage("CpfouCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email","Email já existe"));
 		}
 
 		// inclua os testes aqui, inserindo erros na lista
