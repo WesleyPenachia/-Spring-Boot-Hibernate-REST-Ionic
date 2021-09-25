d62326c88619e5d8d2adb07a7e62f592369ed9bc
Wesley Penachia <wesleyikiko@hotmail.com>
2021-09-25 18:56:17 -0300

    Insercao do Pedido

--- src/main/java/com/natymorgs/backEndSpring/domain/PagamentoComCartao.java
+++ src/main/java/com/natymorgs/backEndSpring/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.natymorgs.backEndSpring.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.natymorgs.backEndSpring.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
--- src/main/java/com/natymorgs/backEndSpring/services/PedidoService.java
+++ src/main/java/com/natymorgs/backEndSpring/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.natymorgs.backEndSpring.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.natymorgs.backEndSpring.domain.ItemPedido;
+import com.natymorgs.backEndSpring.domain.PagamentoComBoleto;
 import com.natymorgs.backEndSpring.domain.Pedido;
+import com.natymorgs.backEndSpring.domain.enums.EstadoPagamento;
+import com.natymorgs.backEndSpring.repositories.ItemPedidoRepository;
+import com.natymorgs.backEndSpring.repositories.PagamentoRepository;
 import com.natymorgs.backEndSpring.repositories.PedidoRepository;
 import com.natymorgs.backEndSpring.services.exceptions.ObjecNotFoundException;
 
@@ -13,11 +20,43 @@ import com.natymorgs.backEndSpring.services.exceptions.ObjecNotFoundException;
 public class PedidoService {
 	
 	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
 	private PedidoRepository repo;
 	
-	public Pedido find(Integer id) { 
-		 Optional<Pedido> obj = repo.findById(id); 
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
+	@Autowired
+	private ProdutoService produtoService;
+
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjecNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
-		} 
+	}
+
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstant(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstant());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItems()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItems());
+		return obj;
+	}
 }
