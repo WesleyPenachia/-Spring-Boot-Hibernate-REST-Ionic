ede32b5f2db527c5f17da2dca4cb4bda4ff0bb7c
Wesley Penachia <wesleyikiko@hotmail.com>
2021-09-25 00:26:47 -0300

    ERRO

--- src/main/java/com/natymorgs/backEndSpring/services/ClienteService.java
+++ src/main/java/com/natymorgs/backEndSpring/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.natymorgs.backEndSpring.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.natymorgs.backEndSpring.domain.Cidade;
 import com.natymorgs.backEndSpring.domain.Cliente;
+import com.natymorgs.backEndSpring.domain.Endereco;
+import com.natymorgs.backEndSpring.domain.enums.TipoCliente;
 import com.natymorgs.backEndSpring.dto.ClienteDTO;
+import com.natymorgs.backEndSpring.dto.ClienteNewDTO;
 import com.natymorgs.backEndSpring.repositories.ClienteRepository;
+import com.natymorgs.backEndSpring.repositories.EnderecoRepository;
 import com.natymorgs.backEndSpring.services.exceptions.DataIntegretyException;
 import com.natymorgs.backEndSpring.services.exceptions.ObjecNotFoundException;
 
@@ -22,11 +29,22 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
-	public Cliente find(Integer id) { 
-		 Optional<Cliente> obj = repo.findById(id); 
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjecNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
-		} 
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
 	
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
@@ -39,22 +57,37 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		}
-		catch(DataIntegrityViolationException e) {
-			throw new DataIntegretyException("Não é possível excluir devido o dado possuir valores referenciados em outra tabela");
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegretyException("Não é possível excluir porque há entidades relacionadas");
 		}
 	}
 	
-	public List<Cliente> findAll(){
+	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
 	
-	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
 	
-	public Cliente fromDTO(ClienteDTO objDTO ) {
-		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfouCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
